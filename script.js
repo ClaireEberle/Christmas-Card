f40f72b84f2a8acd2be510881de2452d2775c3be
Claire Eberle <claireeberle@Claires-MacBook-Pro.local>
2022-12-26 11:39:23 -0900

    added photos

---
+++ script.js
@@ -0,0 +1,8 @@
+// var img3 = document.body.children[1].children[1].children[1]
+
+// img3.addEventListener('click', function(event){
+//     event.preventDefault();
+//     if (mode == "play") {
+//         mode == "dark"
+//     }
+// })
